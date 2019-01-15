57e7f71a79d7708b8d40c0791d33b26361f9b76d
Charles Sprayberry <cspray@gmail.com>
2019-01-14 22:15:02 -0500

    fix LoopReset for phpunit 7

--- src/LoopReset.php
+++ src/LoopReset.php
@@ -3,11 +3,15 @@
 namespace Amp\PHPUnit;
 
 use Amp\Loop;
-use PHPUnit\Framework\BaseTestListener;
+use PHPUnit\Framework\TestListener;
+use PHPUnit\Framework\TestListenerDefaultImplementation;
 use PHPUnit\Framework\Test;
 
-class LoopReset extends BaseTestListener {
-    public function endTest(Test $test, $time) {
+class LoopReset implements TestListener {
+
+    use TestListenerDefaultImplementation;
+
+    public function endTest(Test $test, float $time) : void {
         Loop::set((new Loop\DriverFactory)->create());
         gc_collect_cycles(); // extensions using an event loop may otherwise leak the file descriptors to the loop
     }
